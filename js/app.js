d39a402dbecbe0a5681ab31699ce609107059ca0
mehmetartun <mehmet@artun.com>
2016-12-04 20:29:39 +0100

    Installs EMBonds_@@.xll

--- js/app.js
+++ js/app.js
@@ -1,38 +1,45 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
-
-
 
 $(document).ready(function(){
-	alert('document ready');
     fin.desktop.main(function(){
-		alert('inside fin main');
-		var Excel = fin.desktop.Excel;
-		Excel.init();
-        installAddIn();
+        //installAddIn();
     });
-	
-
+});
+$("#installbutton").click(function(){
+    fin.desktop.main(function(){
+		installAddIn();
+    });
 });
 
-    function installAddIn() {
-		alert('trying to launch');
-        fin.desktop.System.launchExternalProcess({
-            path: 'emb-addin-packaging',
-            target: 'InstallAddIn.vbs',
-            listener: function (args) {
-				
-                console.log('Installer script completed!');
 
-                if (args.exitCode == 0) {
-                    fin.desktop.System.launchExternalProcess({
-                        target: '%localappdata%\\OpenFin\\shared\\assets\\emb-addin-packaging\\EMBonds_64.xll'
+function installAddIn() {
+    fin.desktop.System.launchExternalProcess({
+        alias: 'embonds-addin',
+        target: 'InstallAddIn.vbs',
+        listener: function (args) {
+            console.log('Installer script completed!');
+			console.log(args);
+
+            if (args.exitCode == 0) {
+
+                fin.desktop.System.launchExternalProcess({
+                    alias: "embonds-addin",
+                    target: "TR-Eurobonds-Enable-Macros.xlsm",
+                    listener: function(code) {
+                            console.log('the exit code', code);
+                        }
+                    },
+                    function() {
+                        console.log('Excel launched');
+                    },
+                    function(){
+                        console.log('Error in launching Excel');
                     });
-                }
-            }
-        });
-    }
 
+            }
+        }
+    });
+}
 
 
--- utils/dirtest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	echo getcwd();
-	chdir(getcwd() . "/../");
-	echo getcwd();
-	
-
-
-?>
